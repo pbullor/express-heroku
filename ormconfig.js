27c9dd668098dd215bde68b289eeafbc1811aa1e
pbullor <pablobullor@gmail.com>
2021-05-31 14:56:48 +0000

    cambio entities

--- ormconfig.js
+++ ormconfig.js
@@ -5,7 +5,7 @@ console.log("process.env.DATABASE_URL", process.env.DATABASE_URL)
 module.exports = {
 	type: "postgres",
 	url: process.env.DATABASE_URL,
-	entities: ["./dist/entities/{.ts,.js}"],
+	entities: [Users],
 	logging: false,
     synchronize: false,
     migrations: ["./dist/migrations/*.js"],
